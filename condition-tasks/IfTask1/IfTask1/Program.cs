c37b50f822c8d72747e8092b2b1038c5ad45440f
TeemuHe <teemuhoo@luukku.com>
2018-09-19 09:10:10 +0300

    Condition tasks folder defined. Task 1 defined.

---
+++ condition-tasks/IfTask1/IfTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IfTask1
+{
+    // One line comment
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ohjelma selvittää, onko annettu luku " +
+                "positiivinen, " +
+                "negatiivinen vai nolla.");
+            Console.WriteLine("Syötä luku.")          
+            
+        }
+    }
+}
