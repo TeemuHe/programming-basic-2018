2320ccc3730e35e0c5f95615383003533fda3dea
TeemuHe <teemuhoo@luukku.com>
2018-10-04 08:18:25 +0300

    LoopTask1 updated

--- condition-tasks/LoopTask1/LoopTask1/Program.cs
+++ condition-tasks/LoopTask1/LoopTask1/Program.cs
@@ -6,7 +6,21 @@ namespace LoopTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa luvun kertoman.");
+            Console.Write("Syötä luku, joka on min 1: ");
+
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isnumber = int.TryParse(userInput, out number);
+            if (isnumber)
+            {
+                if (number <= 1)
+                    Console.WriteLine("Väärä syöte!");
+
+
+            }
+
+
         }
     }
 }
