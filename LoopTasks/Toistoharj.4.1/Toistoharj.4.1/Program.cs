75f10b969d3feaa1dc9d44cda992f6ea9508a40f
TeemuHe <teemuhoo@luukku.com>
2018-10-04 08:48:23 +0300

    Do-While comment added

--- LoopTasks/Toistoharj.4.1/Toistoharj.4.1/Program.cs
+++ LoopTasks/Toistoharj.4.1/Toistoharj.4.1/Program.cs
@@ -29,6 +29,15 @@ namespace Toistoharj._4._1
                 //Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
                 //i = i + 1; //i++
             }
+
+
+            //int i = 0;
+            //do
+            //{
+            //rndNumber = rnd.Next(0, 100);
+            //Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
+            //i = i + 1; //i++
+            //} while (i < 100);
         }
 
     }
