3aeda47a28cd025e4576b2d21275cd7c5e85f420
TeemuHe <teemuhoo@luukku.com>
2018-10-04 10:12:10 +0300

    While added

---
+++ LoopTasks/LoopTask3_2/LoopTask3_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LoopTask3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen!");
+            int number = 0;
+            int i = 1;
+            int sum = 1;
+
+            //Check that number is positive
+
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("Virheellinen syöte!");
+                }
+            } while (number <= 0);
+
+            //Calculate sum 1 + 2 + 3 + ...+ N
+            while (i <= number)
+            {
+                sum = sum + 1;
+                i = i + 1;
+            }
+            Console.WriteLine($"{number} ensimmäisen luvun summa = {sum}");
+
+           
+        }
+    }
+}
