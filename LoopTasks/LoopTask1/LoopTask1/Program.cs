5a5135d2b312f99d6af69f031d830790af6e487a
TeemuHe <teemuhoo@luukku.com>
2018-10-04 08:28:10 +0300

    LoopTasks commited

---
+++ LoopTasks/LoopTask1/LoopTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa luvun kertoman.");
+            Console.Write("Syötä luku, joka on vähintään 1: ");
+
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isnumber = int.TryParse(userInput, out number);
+            if (isnumber)
+            {
+                if (number <= 1)
+                    Console.WriteLine("Väärä syöte!");
+            
+
+
+            }
+
+
+        }
+    }
+}
