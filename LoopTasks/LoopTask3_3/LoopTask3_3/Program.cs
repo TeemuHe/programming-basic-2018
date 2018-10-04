ff04ceeeac3851e00821d061117ab16241ad1ddf
TeemuHe <teemuhoo@luukku.com>
2018-10-04 10:48:02 +0300

    LoopTask 3.3 completed

---
+++ LoopTasks/LoopTask3_3/LoopTask3_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.WriteLine("Ohjelma laskee parillisten ja parittomien lukujen summan!");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            while(i <= number)
+            {
+                if(i % 2 == 0)
+                {
+                    evenSum = evenSum + i;  //evenSum += i;
+                }
+                else
+                {
+                    oddSum = oddSum + i;    //oddSum += i;
+                }
+                i++;                        //Yhteinen tekijä molemmissa if ja else
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}");
+            Console.WriteLine($"Parittomien summa on {oddSum}");
+
+        }
+    }
+}
