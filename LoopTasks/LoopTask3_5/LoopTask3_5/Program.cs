bb63096f770783b53486634a34322c9488cd5bc8
TeemuHe <teemuhoo@luukku.com>
2018-10-04 19:52:12 +0300

    LoopTask 3.5 finished

---
+++ LoopTasks/LoopTask3_5/LoopTask3_5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace LoopTask3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.WriteLine("Ohjelma laskee parillisten ja parittomien lukujen summan!");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                
+                    while (i <= number)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            evenSum = evenSum + i;  //evenSum += i;
+                        }
+                        else
+                        {
+                            oddSum = oddSum + i;    //oddSum += i;
+                        }
+                        i++;                        //Yhteinen tekijä molemmissa if ja else
+
+                    }
+
+                        Console.WriteLine($"Parillisten summa on {evenSum}");
+                        Console.WriteLine($"Parittomien summa on {oddSum}");
+
+            }
+                        
+
+
+            else if (number <= 0)
+            {
+                
+                    while (i >= number)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            evenSum = evenSum + i;  //evenSum += i;
+                        }
+                        else
+                        {
+                            oddSum = oddSum + i;    //oddSum += i;
+                        }
+                        i--;                        //Yhteinen tekijä molemmissa if ja else 
+                    }
+
+                        Console.WriteLine($"Parillisten summa on {evenSum}");
+                        Console.WriteLine($"Parittomien summa on {oddSum}");
+
+            }
+                   
+        }
+    }
+}
