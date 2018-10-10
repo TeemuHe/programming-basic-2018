c33c9950d3599c4c0e1864ca1de9cf4b841c58a7
TeemuHe <teemuhoo@luukku.com>
2018-10-10 09:37:58 +0300

    Loop Task 4.2 finished

---
+++ LoopTasks/LoopTask4.2/LoopTask4.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopTask4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1 - 9.");
+
+            int number1 = 1;
+            int number2 = 1;
+            int answer = 1;
+
+            for (number1 = 1; number1 <= 9; number1++)
+            {
+                for (number2 = 1; number2 <= 9; number2++)
+                {
+                    answer = number1 * number2;
+                    Console.WriteLine($"{number1} * {number2} = {answer}");
+                }
+            }
+        }
+    }
+}
