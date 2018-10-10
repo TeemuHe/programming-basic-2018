e9e5ca0b06c1056152ac1f613c3e05fb123b460e
TeemuHe <teemuhoo@luukku.com>
2018-10-10 09:53:33 +0300

    LoopTask1.3 initialized

---
+++ LoopTasks/LoopTask4_3/LoopTask4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LoopTask4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0 - 50."+
+                "Jokaiselle tulostetulle riville tulostetaan 5 lukua pilkulla erottaen");
+
+        }
+    }
+}
