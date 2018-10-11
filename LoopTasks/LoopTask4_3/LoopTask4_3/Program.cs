5061de3bafb7bbe12f405c9e0829ba861c08640c
TeemuHe <teemuhoo@luukku.com>
2018-10-11 13:27:31 +0300

    LoopTask4.3 results appear on the same row

--- LoopTasks/LoopTask4_3/LoopTask4_3/Program.cs
+++ LoopTasks/LoopTask4_3/LoopTask4_3/Program.cs
@@ -6,8 +6,25 @@ namespace LoopTask4_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0 - 50."+
-                "Jokaiselle tulostetulle riville tulostetaan 5 lukua pilkulla erottaen");
+            Random rnd = new Random();
+            int number = 1;
+
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0 - 50.\n" +
+                "Jokaiselle tulostetulle riville tulostetaan 5 lukua pilkulla erottaen.");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Rivi {i} ");
+                for (int j = 1; j <= 5; j++)
+                {
+                    number = rnd.Next(0, 51);
+                    if (j == 5)
+                        Console.WriteLine($"{number}");
+                    else
+                        Console.Write($"{number},");
+                }
+            }
+
 
         }
     }
