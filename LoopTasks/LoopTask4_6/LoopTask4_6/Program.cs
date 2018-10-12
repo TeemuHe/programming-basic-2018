5339306952e0a61b91a0f51e2b7526ca90b08feb
TeemuHe <teemuhoo@luukku.com>
2018-10-12 21:11:40 +0300

    LoopTask4.6 initialized

---
+++ LoopTasks/LoopTask4_6/LoopTask4_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma simuloi 1000 nopanheittoa, ja laskee kutosten määrän.");
+        }
+    }
+}
