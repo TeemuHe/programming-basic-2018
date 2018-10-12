20facc3e607734bf7752942aebfb5f70405bd679
TeemuHe <teemuhoo@luukku.com>
2018-10-12 20:13:12 +0300

    LoopTask4.4 Shows coin flipping results

--- LoopTasks/LoopTask4_4/LoopTask4_4/Program.cs
+++ LoopTasks/LoopTask4_4/LoopTask4_4/Program.cs
@@ -6,7 +6,32 @@ namespace LoopTask4_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma simuloi kolikonheiton tuloksen.");
+            Console.WriteLine("Ohjelma simuloi kolikonheiton tuloksen." +
+                "\nKolikkoa heitetään 20 kertaa.");
+
+            int heads = 0;
+            int tails = 0;
+
+            Random rnd = new Random();
+
+            double flip;
+
+            //Console.Write("Syötä luku, montako kertaa kolikkoa heitetään? ");
+            
+            for (int i = 0; i != 20; i++)
+            {
+                flip = rnd.NextDouble();
+                if (flip > 0.5)
+                {
+                    heads = heads + 1;
+                }
+                else
+                {
+                    tails = tails + 1;
+                }
+
+            }
+            Console.WriteLine($"\nKruunien määrä {heads} ja klaavojen määrä {tails}.");
         }
     }
 }
