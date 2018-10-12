2c1e8391b80011c3fc911bb3d48fae4140867e9d
TeemuHe <teemuhoo@luukku.com>
2018-10-12 21:09:09 +0300

    LoopTask4.5 finished

--- LoopTasks/LoopTask4_5/LoopTask4_5/Program.cs
+++ LoopTasks/LoopTask4_5/LoopTask4_5/Program.cs
@@ -6,7 +6,29 @@ namespace LoopTask4_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa vakioveikkausrivin");
+            Random rnd = new Random();
+
+            for (int i = 1; i != 14; i++)
+            {
+                Console.Write($"{i}.  ");
+                double x = rnd.Next(100);
+
+                if(x <= 40)
+                {
+                    Console.WriteLine("1");
+                }
+
+                else if(x > 40 && x < 60)
+                {
+                    Console.WriteLine("X");
+                }
+
+                else
+                {
+                    Console.WriteLine("2");
+                }
+            }
         }
     }
 }
