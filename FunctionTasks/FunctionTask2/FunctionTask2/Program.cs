503fd8d59b77d3bd399685179b4308a3f70c38ff
TeemuHe <teemuhoo@luukku.com>
2018-10-16 15:11:48 +0300

    FunctionTask2 initialized

---
+++ FunctionTasks/FunctionTask2/FunctionTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma palauttaa kahdesta annetusta luvusta pienemmän.");
+        }
+    }
+}
