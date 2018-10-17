1de672d42412ac35922240f7a9d9a12d6da9dba8
TeemuHe <teemuhoo@luukku.com>
2018-10-17 13:57:36 +0300

    FunctionTask2 completed

--- FunctionTasks/FunctionTask2/FunctionTask2/Program.cs
+++ FunctionTasks/FunctionTask2/FunctionTask2/Program.cs
@@ -7,6 +7,23 @@ namespace FunctionTask2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma palauttaa kahdesta annetusta luvusta pienemmän.");
+            Console.Write("Syötä luku: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Syötä toinen luku: ");
+            int number2 = int.Parse(Console.ReadLine());
+            Compare(number1, number2);
         }
+        public static void Compare(int number1, int number2)
+        {
+            if (number1 < number2)
+            {
+                Console.WriteLine($"Luku {number1} on pienempi kuin luku {number2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Luku {number2} on pienempi kuin luku {number1}.");
+            }
+        }
+
     }
 }
