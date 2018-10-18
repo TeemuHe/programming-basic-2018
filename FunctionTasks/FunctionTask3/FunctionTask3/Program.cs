ff9eae4386b06220d69a27b9ec7a8968c0493d7c
TeemuHe <teemuhoo@luukku.com>
2018-10-18 21:26:58 +0300

    FunctionTask3 initialized

---
+++ FunctionTasks/FunctionTask3/FunctionTask3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FunctionTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nmbr;
+            NumChecker(int nmbr);
+        }
+
+        private static int NumChecker(int number)
+        {
+            for (int i = 0; i < 1;)
+            {
+                Console.WriteLine("Ohjelma katsoo onko syötetty luku väliltä 1-10.");
+                Console.Write("Syötä luku: ");
+                i = int.Parse(Console.ReadLine());
+
+                if (i > 1 && i < 11)
+                {
+                    Console.WriteLine($"Luku {i} on väliltä 1-10.");
+                }
+                else if (i > 10 && i < 1)
+                {
+                    Console.Write($"Syötä luku väliltä 1-10.");
+                    i++;
+                }
+            }
+        }
+    }
+}
