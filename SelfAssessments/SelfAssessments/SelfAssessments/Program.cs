85d97bcb99db71d50f64688d5bfb8f1ddcb8ae7a
TeemuHe <teemuhoo@luukku.com>
2018-10-23 16:20:36 +0300

    SelfAssessment1 done

---
+++ SelfAssessments/SelfAssessments/SelfAssessments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SelfAssessments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa syötetyn sanan tai lauseen" +
+                "viisi kertaa.");
+            Console.Write("Kirjoita sana tai lause: ");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}. {userInput}");
+            }
+        }
+    }
+}
