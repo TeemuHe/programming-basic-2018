78ac2c84538029b51ecee6ef8209d4e453c0e4c3
TeemuHe <teemuhoo@luukku.com>
2018-10-26 22:16:57 +0300

    SelfAssessment3 done

---
+++ SelfAssessments/SelfAssessment3/SelfAssessment3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelfAssessment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee luvut yhteen, kunnes luku -1 syötetään." +
+                "Ohjelma tulostaa syötettyjen lukujen summan.");
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("Syötä luku: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                if (isNumber && userInput != -1)
+                {
+                    sum += userInput;
+                }
+                else if (isNumber && userInput == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen syöte!");
+                }
+            }
+            Console.WriteLine($"Syötteiden summa on {sum}.");
+        }
+    }
+}
