c5e19f206483eee346f132aa853087635a8712d0
TeemuHe <teemuhoo@luukku.com>
2018-10-31 14:48:16 +0200

    ArrayTask2Lotto initialized

---
+++ ArrayTasks/ArrayTask2Lotto/ArrayTask2Lotto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayTask2Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 7 eri lukua 1...40 ja tulostaa arvotut luvut pienimmästä suurimpaan.");
+            int[] numbers = new int[7];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(40);
+            }
+
+        }
+    }
+}
