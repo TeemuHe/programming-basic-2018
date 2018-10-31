f87b92b3690a426a0a66d35b17f3b76b2929273d
TeemuHe <teemuhoo@luukku.com>
2018-10-31 13:33:21 +0200

    ArrayTask1 done

--- ArrayTasks/ArrayTask1/ArrayTask1/Program.cs
+++ ArrayTasks/ArrayTask1/ArrayTask1/Program.cs
@@ -6,7 +6,18 @@ namespace ArrayTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa tuotteiden nimet ja yhteishinnat näytölle.");
+            decimal[] productPrice = new decimal[] { 7.96m, 72.00m, 99.90m };
+            int[] productAmount = new int[] { 1, 3, 2 };
+            decimal[] productTotalPrice = new decimal[3];
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote {i + 1}. {productTotalPrice[i]:c}");
+
+            }
         }
     }
 }
