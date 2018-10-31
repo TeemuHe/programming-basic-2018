b159ec71a26553f29b1c89ec8b7880ec0c4045a3
TeemuHe <teemuhoo@luukku.com>
2018-10-31 13:36:14 +0200

    ArrayTask 2 initialized

---
+++ ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 100 lukua vällltä 0...50 ja tulostaa ne sekä antaa niiden keskiarvon.");
+        }
+    }
+}
