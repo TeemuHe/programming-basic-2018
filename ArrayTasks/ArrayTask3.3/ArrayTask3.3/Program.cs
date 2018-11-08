dd627349883e232c3ab50bee24c41ac1bea919a2
TeemuHe <teemuhoo@luukku.com>
2018-11-08 10:50:10 +0200

    ArrayTask3.3 initialized

---
+++ ArrayTasks/ArrayTask3.3/ArrayTask3.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayTask3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tuloostaa 3 kokonaisluku taulukkoa, joissa jokaisessa on 10 lukua.");
+        }
+    }
+}
