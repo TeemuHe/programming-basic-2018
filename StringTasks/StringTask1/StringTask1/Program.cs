b6b527c7fc7ba1ebc5446fc18ac4dfa39dc24494
TeemuHe <teemuhoo@luukku.com>
2018-11-12 08:50:28 +0200

    edited

--- StringTasks/StringTask1/StringTask1/Program.cs
+++ StringTasks/StringTask1/StringTask1/Program.cs
@@ -6,6 +6,7 @@ namespace StringTask1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ohjelma laskee merkkien määrän.");
             Console.Write("Syötä teksti: ");
             string text = Console.ReadLine();
             Console.WriteLine(text.Length);
