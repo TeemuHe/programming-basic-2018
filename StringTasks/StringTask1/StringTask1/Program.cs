7a12ae354b0065cefe2bb92b7d38bbe9dc0b9a33
TeemuHe <teemuhoo@luukku.com>
2018-11-12 08:44:03 +0200

    StringTask1 done

---
+++ StringTasks/StringTask1/StringTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä teksti: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Length);
+        }
+    }
+}
