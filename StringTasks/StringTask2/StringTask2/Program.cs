3b3c04ad11b08e844c02eaa21963176dd7594989
TeemuHe <teemuhoo@luukku.com>
2018-11-12 09:00:01 +0200

    StringTask2 done

---
+++ StringTasks/StringTask2/StringTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa e-merkit @-merkeiksi.");
+            Console.Write("Syötä teksti: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Replace("e","@"));
+        }
+    }
+}
