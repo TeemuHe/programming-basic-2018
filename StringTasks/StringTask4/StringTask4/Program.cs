3ff16f5766ebf90c30b4f6e66216ee8e842e38b3
TeemuHe <teemuhoo@luukku.com>
2018-11-12 11:51:39 +0200

    done

--- StringTasks/StringTask4/StringTask4/Program.cs
+++ StringTasks/StringTask4/StringTask4/Program.cs
@@ -9,7 +9,23 @@ namespace StringTask4
             Console.WriteLine("Ohjelma tarkastaa, onko syötetty sana tai lause palindromi.");
             Console.Write("Syötä sana tai lause: ");
             string text = Console.ReadLine().ToUpper();
-            Console.WriteLine(text);
+            bool isPalindrome = true;
+            for (int i = 0; i < text.Length/2; i++)
+            {
+                if (text[i] != text[text.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine($"Sana {text} on palindromi");
+            }
+            else
+                Console.WriteLine($"Sana {text} ei ole palindromi.");
+
         }
     }
 }
