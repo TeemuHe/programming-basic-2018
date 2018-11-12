bb09a8d5c649019f4b2294d7a0881073d1b5fcfa
TeemuHe <teemuhoo@luukku.com>
2018-11-12 10:23:54 +0200

    done

--- ArrayTasks/ArrayTask3.1/ArrayTask3.1/Program.cs
+++ ArrayTasks/ArrayTask3.1/ArrayTask3.1/Program.cs
@@ -6,7 +6,18 @@ namespace ArrayTask3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma tulostaa yksiulotteisen taulukon [0,20[.");
+            Console.WriteLine("Ohjelma tulostaa yksiulotteisen taulukon.");
+            Console.WriteLine("[X] = [arvo]");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(20);
+            }
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine($"[{j}] = {numbers[j]:D2}");
+            }
         }
     }
 }
