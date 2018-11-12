3fc09b9de8771eb89671d8eb9356d421b9840cc6
TeemuHe <teemuhoo@luukku.com>
2018-11-12 09:54:59 +0200

    StringTask3 done

---
+++ StringTasks/StringTask3/StringTask3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee L-kirjaimet syötetystä tekstistä.");
+            Console.Write("Syötä teksti: ");
+            string text = Console.ReadLine().ToUpper();
+            int counter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(text [i] == 'L')
+                {
+                    counter++;
+                }                
+            }
+            Console.WriteLine($"Syötteessä {text} on {counter} L-kirjainta.");                    
+        }
+    }
+}
