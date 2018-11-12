64514da09f05546de2350bdb45f0e29a78b42cf1
TeemuHe <teemuhoo@luukku.com>
2018-11-12 11:18:38 +0200

    ArrayTask3.2 done

--- ArrayTasks/ArrayTask3.2/ArrayTask3.2/Program.cs
+++ ArrayTasks/ArrayTask3.2/ArrayTask3.2/Program.cs
@@ -6,7 +6,25 @@ namespace ArrayTask3._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa kaksiulotteisen taulukon (10,20).");
+            int widht = 10; int hight = 20;
+            int[,] array = new int[widht, hight];
+            Random rnd = new Random();
+
+            for (int i = 0; i < widht; i++)
+            {
+                for (int j = 0; j < hight; j++)
+                {
+                    array[i, j] = rnd.Next(100);
+                }
+            }
+            for (int i = 0; i < widht; i++)
+            {
+                for (int j = 0; j < hight; j++)
+                {
+                    Console.WriteLine($"[{i:d2},{j:d2}] = {array [i,j]:d2}");
+                }
+            }
         }
     }
 }
