311654baa02b50b3fe99ac78d3a2446bb99fdabe
TeemuHe <teemuhoo@luukku.com>
2018-11-12 11:19:48 +0200

    StringTask4 initialized

---
+++ StringTasks/StringTask4/StringTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa, onko syötetty sana tai lause palindromi.");
+            Console.Write("Syötä sana tai lause: ");
+            string text = Console.ReadLine().ToUpper();
+            Console.WriteLine(text);
+        }
+    }
+}
