e7f96e1fdea05a5d293dd010c2f0e7afd71f8a0c
TeemuHe <teemuhoo@luukku.com>
2018-11-15 17:02:10 +0200

    StringTaskVocal initialized

---
+++ StringTasks/StringTaskVocal/StringTaskVocal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringTaskVocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä tekstistä vokaalien määrän.");
+            Console.Write("Syötä teksti: ");
+            string text = Console.ReadLine();
+        }
+    }
+}
