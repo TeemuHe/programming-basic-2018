a07f36b69cb1e60b8347ec21b57fac715cd393b3
TeemuHe <teemuhoo@luukku.com>
2018-11-15 17:57:42 +0200

    Calculator done

--- StringTasks/StringTaskVocal/StringTaskVocal/Program.cs
+++ StringTasks/StringTaskVocal/StringTaskVocal/Program.cs
@@ -8,7 +8,18 @@ namespace StringTaskVocal
         {
             Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä tekstistä vokaalien määrän.");
             Console.Write("Syötä teksti: ");
-            string text = Console.ReadLine();
+            string input = Console.ReadLine().ToLower();
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y' || input[i] == 'ä' || input[i] == 'ö')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Tekstissä {input} on {count} vokaalia.");
         }
     }
 }
