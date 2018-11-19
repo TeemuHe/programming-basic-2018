d7f2bfb17c40c9569f10f88bbec24e35ff5be911
TeemuHe <teemuhoo@luukku.com>
2018-11-19 08:48:12 +0200

    .

--- StringTasks/StringTaskVowelCounter/StringTaskVowelCounter/Program.cs
+++ StringTasks/StringTaskVowelCounter/StringTaskVowelCounter/Program.cs
@@ -13,7 +13,20 @@ namespace StringTaskVowelCounter
         static string UserInput()
         {
             Console.Write("Syötä teksti: ");
-            return Console.ReadLine();
+            return Console.ReadLine().ToUpper();
+        }
+
+        static string VowelsRemove(string d)
+        {
+            for (int i = 0; i < d.Length; i++)
+            {
+                if(d[i] == 'A' || d[i] == 'E' || d[i] == 'I' || d[i] == 'O' || d[i] == 'U' || d[i] == 'Y' || d[i] == 'Ä' || d[i] == 'Ö')
+                {
+                    //d.Replace(d[i].ToString(), "");
+                    d = d.Remove(i, 1);
+                    i--;
+                }
+            }
         }
     }
 }
