d77d7fa125c21bad69ed3877fdcfcc4589e067d4
TeemuHe <teemuhoo@luukku.com>
2018-11-19 09:20:20 +0200

    Empty lines deleted

--- StringTasks/StringTaskVowel/StringTaskVocal/Program.cs
+++ StringTasks/StringTaskVowel/StringTaskVocal/Program.cs
@@ -13,7 +13,6 @@ namespace StringTaskVowel
 
             for (int i = 0; i < input.Length; i++)
             {
-
                 if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y' || input[i] == 'ä' || input[i] == 'ö')
                 {
                     count++;
