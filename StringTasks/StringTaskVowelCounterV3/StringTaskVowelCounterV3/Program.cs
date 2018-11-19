da8c14644a3cf2cc271cf192263f26db85aac89d
TeemuHe <teemuhoo@luukku.com>
2018-11-19 09:27:29 +0200

    .

--- StringTasks/StringTaskVowelCounterV3/StringTaskVowelCounterV3/Program.cs
+++ StringTasks/StringTaskVowelCounterV3/StringTaskVowelCounterV3/Program.cs
@@ -12,7 +12,6 @@ namespace StringTaskVowel
             int count = 0;
             for (int i = 0; i < input.Length; i++)
             {
-
                 if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y' || input[i] == 'ä' || input[i] == 'ö')
                 {
                     count++;
