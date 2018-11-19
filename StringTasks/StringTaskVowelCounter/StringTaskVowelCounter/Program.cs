3049bf09e30c84d08913a16617c85fc777b23fea
TeemuHe <teemuhoo@luukku.com>
2018-11-19 08:46:32 +0200

    VowelCounter initialized

---
+++ StringTasks/StringTaskVowelCounter/StringTaskVowelCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringTaskVowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo syötteessä olevien vokaalien määrän.");
+            string userInput = UserInput();
+        }
+
+        static string UserInput()
+        {
+            Console.Write("Syötä teksti: ");
+            return Console.ReadLine();
+        }
+    }
+}
