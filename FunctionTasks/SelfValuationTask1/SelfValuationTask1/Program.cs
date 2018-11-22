9679a24fffb5bb725f5693ee77fb85d243b87b2d
TeemuHe <teemuhoo@luukku.com>
2018-11-22 08:48:19 +0200

    Solution 1 done

--- FunctionTasks/SelfValuationTask1/SelfValuationTask1/Program.cs
+++ FunctionTasks/SelfValuationTask1/SelfValuationTask1/Program.cs
@@ -6,7 +6,26 @@ namespace SelfValuationTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa tähtiä.");
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                PrintStars(numbers[i]);
+            }
+        }
+        /// <summary>
+        /// Tulostaa tähtiä
+        /// length = tähtien määrä
+        /// </summary>
+        /// <param name="length"></param>
+        static void PrintStars(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
         }
     }
 }
