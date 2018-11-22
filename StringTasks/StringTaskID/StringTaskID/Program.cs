dc9abd26fee4282054993cc4446958fd634d79be
TeemuHe <teemuhoo@luukku.com>
2018-11-22 09:21:02 +0200

    Task initialized

---
+++ StringTasks/StringTaskID/StringTaskID/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringTaskID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa henkilötunnuksen oikeellisuuden.");
+
+        }
+    }
+}
