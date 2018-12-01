233301d6a4143e5813fad1dd2cf481f28a4e771a
TeemuHe <teemuhoo@luukku.com>
2018-12-01 21:55:47 +0200

    Hetu2 initialized

---
+++ StringTasks/Hetu2/Hetu2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hetu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+        }
+        static string Intro()
+        {
+
+        }
+    }
+}
