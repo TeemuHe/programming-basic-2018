263c71b5cfb39d12f9d98fb468dcfa4c68af6f79
TeemuHe <teemuhoo@luukku.com>
2018-12-03 08:47:55 +0200

    Task initialized

---
+++ ReferenceNumberTask/ReferenceNumber1/ReferenceNumber1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReferenceNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa kotimaisen viitenumeron.");
+        }
+    }
+}
