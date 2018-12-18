45639e7023fb8ab9cce580122cd24a26e3974484
TeemuHe <teemuhoo@luukku.com>
2018-12-18 15:30:20 +0200

    new reference number function created

--- ReferenceNumberTask/ReferenceNumber1/ReferenceNumber1/Program.cs
+++ ReferenceNumberTask/ReferenceNumber1/ReferenceNumber1/Program.cs
@@ -23,14 +23,14 @@ namespace ReferenceNumber1
                             break;
                         case "L":
                             CreateRef(path);
-                            msg = "\nTiedoston loppuun kirjoitus on päättynyt! \nPaina Enter jatkaaksesi!";
+                            msg = "\nViitenumero luotu! \nPaina Enter jatkaaksesi!";
                             break;
                         case "N":
                             CreateRefs(path);
-                            msg = "\nTiedoston lukeminen on päättynyt! \nPaina Enter jatkaaksesi!";
+                            msg = "\nHaluttu määrä viitenumeroita luotu! \nPaina Enter jatkaaksesi!";
                             break;
-                        case "S":
-                            ShowFile(path);
+                        case "R":
+                            ReadFile(path);
                             msg = "\nTiedoston lukeminen on päättynyt! \nPaina Enter jatkaaksesi!";
                             break;
                         case "X":
@@ -55,7 +55,7 @@ namespace ReferenceNumber1
             Console.WriteLine("[K] Tarkastaa viitenumeron oikeellisuuden");
             Console.WriteLine("[L] Luodaan yksi viitenumero");
             Console.WriteLine("[N] Luodaan haluttu määrä viitenumeroita");
-            Console.WriteLine("[S] Näytä luodut viitenumerot");
+            Console.WriteLine("[R] Näytä luodut viitenumerot");
             Console.WriteLine("[X] Lopeta ohjelmansuoritus");
             Console.WriteLine();
             Console.Write("Valitse mitä tehdään: ");
@@ -95,7 +95,7 @@ namespace ReferenceNumber1
             }
         }
         /// <summary>
-        /// Luodaan numerosarja ilman tarkastusnumeroa
+        /// Luodaan viitenumero ilman tarkastusnumeroa
         /// </summary>
         /// <param name="refNumber"></param>
         /// <returns></returns>
@@ -138,6 +138,119 @@ namespace ReferenceNumber1
             {
                 Console.WriteLine("Virheellinen viitenumero!");
             }
+        }
+        static void CreateRef(string filePath)
+        {
+            string refNumberCreate = Intro2();
+            bool isReal = Length2(refNumberCreate);
+            int[] factory = new int[] { 7, 3, 1 };
+            
+            int checkNumber = CheckNumberAdd(refNumberCreate, factory);
+        }
+        static string Intro2()
+        {
+            Console.WriteLine("Luo uuden kotimaisen viitenumeron.");
+            Console.Write("Syötä viitenumero ilman tarkastusnumeroa: ");
+            return Console.ReadLine();
+        }
+        /// <summary>
+        /// Tarkastaa onko viitenumero sallitun pituinen
+        /// </summary>
+        /// <param name="refNumberCreate"></param>
+        /// <returns></returns>
+        static bool Length2(string refNumberCreate)
+        {
+            if (refNumberCreate.Length > 2 && refNumberCreate.Length < 18)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Virheellisen pituinen viitenumero!");
+                return false;
+            }
+        }
+        /// <summary>
+        /// Luo tarkastusnumeron ja liittää sen viitenumeron perään
+        /// </summary>
+        /// <returns></returns>
+        static int CheckNumberAdd(string refNumberCreate, int[] factory)
+        {
+            int sum = 0;
+            int j = refNumberCreate.Length - 1;
+            for (int i = 0; i < refNumberCreate.Length; i++)
+            {
+                int nbr = Convert.ToInt32(refNumberCreate[j].ToString());
+                sum += nbr * factory[i % 3];
+                j--;
+            }
+            int checkNumber = 10 - (sum % 10);
+            if (checkNumber == 10)
+            {
+                checkNumber = 0;
+            }
+            refNumberCreate += checkNumber;
+            Console.WriteLine($"{refNumberCreate}");
+            return checkNumber;
+        }
+        /// <summary>
+        /// Lukee aiemmin syötetyt viitenumerot tiedostosta referencenumber.txt
+        /// </summary>
+        /// <param name="filePath"></param>
+        static void ReadFile(string filePath)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line = null;
+
+                    Console.WriteLine("Tiedostosta löytyy seuraavat rivit:\n");
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tapahtui virhe: {ex.Message}");
+            }           
+        }
+        static void CreateRefs(string filePath)
+        {
+            string amount = Intro3();
+            string basePart = BasePart();
+
+
+        }
+        /// <summary>
+        /// Kerrotaan kuinka monta viitenumeroa halutaan
+        /// </summary>
+        /// <returns></returns>
+        static string Intro3()
+        {
+            Console.Write("Kuinka monta kotimaista viitenumeroa tulostetaan? ");
+            return Console.ReadLine();
+        }
+        /// <summary>
+        /// Tallennetaan viitenumeron alku
+        /// </summary>
+        /// <returns></returns>
+        static string BasePart()
+        {
+            Console.Write("Syötä haluamasi viitenumeron alku: ");
+            return Console.ReadLine();
+        }
+        static string AddNumbers(int amount, string basePart)
+        {
+            for (int i = 0; i < amount-1; i++)
+            {
+                basePart += amount;
+                Console.WriteLine($"{basePart}");
+            }
+            return basePart;
         }
     }
 }
